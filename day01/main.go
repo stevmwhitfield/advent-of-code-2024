a242079196dfab3e170eabbe296a7f2bb398d07e
Steven Whitfield <steven.whitfield@cognizant.com>
2024-12-02 14:54:50 -0700

    day 1 part 1 solution

--- day01/main.go
+++ day01/main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
 
 func main() {
-	fmt.Println("hello world")
+	cwd, err := os.Getwd();
+	check(err)
+
+	path := filepath.Join(cwd, "inputs", "01.txt")
+
+	file, err := os.Open(path)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	
+	var left []int
+	var right []int
+	
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		a, _ := strconv.Atoi(parts[0])
+		b, _ := strconv.Atoi(parts[1])
+		left = append(left, a)
+		right = append(right, b)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	sort.Ints(left)
+	sort.Ints(right)
+
+	sum := 0
+	for i := range left {
+		if left[i] > right[i] {
+			sum += left[i] - right[i]
+		} else {
+			sum += right[i] - left[i]
+		}
+	}
+
+	fmt.Println(sum) // 1222801
 }
